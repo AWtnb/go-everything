99f46e3a948d05543b8a878f244951a65f0f7474
awtnb <arikaebanataw@gmail.com>
2024-04-16 13:19:11 +0900

    add test

---
+++ everything_test.go
@@ -0,0 +1,18 @@
+package everything_test
+
+import (
+	"testing"
+
+	"github.com/AWtnb/go-everything"
+)
+
+func TestEverything(t *testing.T) {
+	found, err := everything.Scan(`C:\Personal\gotemp`, false)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	for _, s := range found {
+		t.Logf("'%s' was found", s)
+	}
+}
